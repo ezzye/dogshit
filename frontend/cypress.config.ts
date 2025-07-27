a3fc96a9781ecd57484e32be56854c56c102dccb
Errol Elliott <ezzy.elliott@gmail.com>
2025-07-28 00:12:52 +0100

    feat(dev): add docker service for vite

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://frontend:5173',
     supportFile: false,
   },
 });
